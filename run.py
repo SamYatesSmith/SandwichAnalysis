53787091ab8455fe971a3e0f2b6aa3b72d956d67
Sam Yates-Smith <samyatessmith@gmail.com>
2024-02-28 15:35:27 +0000

    Adding single function which carries out the update_sales and update_surplus functions

--- run.py
+++ run.py
@@ -47,21 +47,29 @@ def validate_data(values):
 
     return True
 
-def update_sales_worksheet(data):
-    """Update sales worksheet, add new row with the user inputed list"""
+# def update_sales_worksheet(data):
+#     """Update sales worksheet, add new row with the user inputed list"""
 
-    print("updating sales worksheet...\n")
-    sales_worksheet = SHEET.worksheet('sales')
-    sales_worksheet.append_row(data)
-    print("Sales worksheet updated successfully!\n")
+#     print("updating sales worksheet...\n")
+#     sales_worksheet = SHEET.worksheet('sales')
+#     sales_worksheet.append_row(data)
+#     print("Sales worksheet updated successfully!\n")
 
-def update_surplus_worksheet(data):
-    """Update surplus worksheet, deduces surplus and adds new row with the user inputed list"""
+# def update_surplus_worksheet(data):
+#     """Update surplus worksheet, deduces surplus and adds new row with the user inputed list"""
 
-    print("updating surplus worksheet...\n")
-    surplus_worksheet = SHEET.worksheet('surplus')
-    surplus_worksheet.append_row(data)
-    print("Surplus worksheet updated successfully!\n")
+#     print("updating surplus worksheet...\n")
+#     surplus_worksheet = SHEET.worksheet('surplus')
+#     surplus_worksheet.append_row(data)
+#     print("Surplus worksheet updated successfully!\n")
+
+def update_worksheet(data, worksheet):
+    """Receives a list of integers to be inserted into a worksheet
+    update the relevant worksheet with the data provided"""
+    print(f"updating {worksheet} worksheet...\n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print("{worksheet} worksheet updated successfully")
 
 def calculate_surplus_data(sales_row):
     """Comparing sales with stock and calculating surplus"""
@@ -79,9 +87,9 @@ def main():
     """Run all programs funciton"""
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_sales_worksheet(sales_data)
+    update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
-    update_surplus_worksheet(new_surplus_data)
+    update_worksheet(new_surplus_data, "surplus")
     
 print("Welcome to Love Sandwiches data automation")
 main()
